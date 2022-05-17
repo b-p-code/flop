95a485769430c57568ad7ef0df7780c37061c838
NickMcSlick <bryce.paubel@gmail.com>
2022-05-17 13:14:29 -0500

    basic command 'say' added

--- js/flopInterpreter.js
+++ js/flopInterpreter.js
@@ -13,8 +13,6 @@ function setupCodeMirror() {
 function run() {
     let text = editor.getValue();
 
-    console.log(text);
-
     if(!text || text === "") {
         console.log("error");
     } else {
@@ -23,5 +21,28 @@ function run() {
 }
 
 function interpretFlop(input) {
+    let cout = document.getElementById("output");
+
     let lines = input.split(/\r?\n/);
+    lines.push(null);
+
+    console.log(lines);
+
+    let i = 0;
+    while (lines[i]) {
+        let output = "";
+        let line = lines[i].split(" ");
+        switch (line[0]) {
+            case "say":
+                for (let i = 1; i < line.length; i++) {
+                    output += line[i] + " ";
+                }
+                cout.value = output;
+                output = "";
+                break;
+            default:
+                cout.value = "error at line " + (i + 1);
+        }
+        i++;
+    }
 }
